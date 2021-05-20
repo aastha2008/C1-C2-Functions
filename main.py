52749ecb115b29074bd827100f9ef88ebf8b247e
BhagatAastha <>
2021-05-20 23:26:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+##Functions
+'''
+A function is a group of related statements that performs a specific task
+Functions help us breakdown our program into sections
+
+### DRY: Don't Repeat Yourself
+
+### Formula
+python
+def functionName(optional parameters):
+  #BODY
+  # optional return statements
+
+  To use a function, we have to _call_ it 
+  funtionName(optional parameters)
+  '''
